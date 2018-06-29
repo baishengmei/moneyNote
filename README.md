d3b3b3297362b9165c59e373bcbe029842a1a40d
Shengmei Bai <1023638368@qq.com>
2018-06-29 23:57:03 +0800

    redux配置：方案一

---
+++ README.md
@@ -0,0 +1,16 @@
+# 部署步骤
+
+### 1. 微信小程序编译预览
+```
+$ npm run dev:weapp
+```
+
+### 2. H5编译预览模式
+```
+npm run dev:h5
+```
+
+
+# 说明几点：
+### 1. config/index.js里面导出用module.exports = 而不能用export default
+### 2. node版本不能太低
---
+++ src/reducers/counter.js
@@ -0,0 +1,43 @@
+// import { ADD, MINUS } from '../constants/counter'
+
+// const INITIAL_STATE = {
+//   num: 0
+// }
+
+// export default function counter (state = INITIAL_STATE, action) {
+//   switch (action.type) {
+//     case ADD:
+//       return {
+//         ...state,
+//         num: state.num + 1
+//       }
+//      case MINUS:
+//        return {
+//          ...state,
+//          num: state.num - 1
+//        }
+//      default:
+//        return state
+//   }
+// }
+
+import { createReducer } from 'redux-immutablejs'
+import { fromJS } from 'immutable'
+import { ADD, MINUS } from '../constants/counter'
+
+export default createReducer(fromJS({
+  num: 0
+}),{
+  [ADD]: (state) => {
+    const counterState = state.toJS()
+    return state.merge({
+      num: counterState.num + 1
+    })
+  },
+  [MINUS]: (state) => {
+    const counterState = state.toJS()
+    return state.merge({
+      num: counterState.num - 1
+    })
+  }
+})
