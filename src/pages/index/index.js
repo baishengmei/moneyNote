cc61ab163be34a10041fd9b51355c7443668733a
Shengmei Bai <1023638368@qq.com>
2018-06-30 00:06:51 +0800

    redux配置：方案二

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,38 +1,38 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text, Button } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
-import { bindActionCreators } from 'redux'
+// import { bindActionCreators } from 'redux'
 import './index.css'
 
-// import { add, minus, asyncAdd } from '../../actions/counter'
-import * as Actions from '../../actions/counter'
+import { add, minus, asyncAdd } from '../../actions/counter'
+// import * as Actions from '../../actions/counter'
 
-function mapStateToProps(state) {
-  return {
-    counter: state.counter.toJS()
-  }
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    ...bindActionCreators(Actions, dispatch)
-  }
-}
-
-@connect(mapStateToProps, mapDispatchToProps)
-
-// @connect(({ counter }) => ({
-//   counter
-// }), (dispatch) => ({
-//   add () {
-//     dispatch(add())
-//   },
-//   dec () {
-//     dispatch(minus())
-//   },
-//   asyncAdd () {
-//     dispatch(asyncAdd())
+// function mapStateToProps(state) {
+//   return {
+//     counter: state.counter.toJS()
 //   }
-// }))
+// }
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     ...bindActionCreators(Actions, dispatch)
+//   }
+// }
+
+// @connect(mapStateToProps, mapDispatchToProps)
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
 
 export default class Index extends Component {
   config = {
@@ -60,7 +60,7 @@ export default class Index extends Component {
       <View className='index'>
         <Text onClick={this.onClick}>世界你好</Text>
         <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.minus}>-</Button>
+        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
         <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
         <View>{this.props.counter.num}</View>
       </View>
